510a589991d3db9c9fa9c4bb2448a2d12c8eeab8
Alexander <dm7209@ukr.net>
2022-08-17 07:57:56 +0300

    fix: convey mongodb uri ot env

--- app.js
+++ app.js
@@ -23,8 +23,7 @@ app.use(compression());
 var mongoose = require('mongoose');
 // var mongoDB = 'mongodb+srv://freecodecamp:freecodecamp@cluster0.jo0ml.mongodb.net/?retryWrites=true&w=majority';
 // Set up mongoose connection
-const dev_db_url = 'mongodb+srv://freecodecamp:freecodecamp@cluster0.jo0ml.mongodb.net/?retryWrites=true&w=majority'
-const mongoDB = process.env.MONGODB_URI || dev_db_url;
+const mongoDB = process.env.MONGODB_URI;
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
